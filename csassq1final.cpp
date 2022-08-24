0c450d150963e2824fc5ead92cc6cba6c2df853d
yash121787 <111965936+yash121787@users.noreply.github.com>
2022-08-24 14:06:15 +0530

    Add files via upload

---
+++ csassq1final.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+#include <cmath>
+#include <climits>
+using namespace std;
+class Array
+{
+	private:
+	int n;
+	int arr[];
+	public:
+	Array(int n)
+	{
+		this->n = n;
+		int arr[this->n];
+	}
+	void input()
+	{
+	cout<<"Enter elements of array"<<endl;
+	for (int i = 0; i < this->n; i++)
+	{
+		cin >> arr[i];
+	}
+		
+	}
+	void display()
+	{
+	for (int i = 0; i < this->n; i++)
+	{
+		cout << arr[i]<<" ";
+	}
+	cout<<endl;
+	}
+
+	void sum()
+	{
+		int add = 0;
+	for (int i = 0; i < this->n; i++)
+	{
+		add += arr[i];
+	}
+	cout<<add<<endl;
+	}
+	int maxelement()
+	{
+	   int mx = INT_MIN;
+	for (int i = 0; i < this->n; i++)
+	{
+		 mx = max(mx,arr[i]);
+	}
+	return mx;
+	}
+	int minelement()
+	{
+	   int mini = INT_MAX;
+	for (int i = 0; i < this->n; i++)
+	{
+		 mini = min(mini,arr[i]);
+	}
+	return mini;
+	}
+	void sort()
+	{
+		for(int i = 0; i < n-1; i++ ) {
+        int minIndex = i;
+        
+        for(int j = i+1; j<n; j++) {
+            
+            if(arr[j] < arr[minIndex]) 
+                minIndex = j;
+            
+        }
+        swap(arr[minIndex], arr[i]);
+    }
+	}
+	void reverse()
+	{   int s = 0;
+	    int e =this->n-1;
+
+    while(s<=e) {
+        swap(arr[s], arr[e]);
+        s++;
+        e--;
+    }
+	}
+};
+int main() {
+	int n;
+	cout<<"Enter size of array"<<endl;
+	cin>>n;
+	Array arr(n);
+	//Taking input in array
+	arr.input();
+	//Displaying array
+	arr.display();
+	//Sum of array
+	// int sum_of_array = arr.sum();
+	 cout<<"Sum of the array is ";
+     arr.sum();
+
+	cout<<"Max element is "<<arr.maxelement()<<endl;
+	cout<<"Min element is "<<arr.minelement()<<endl;
+
+	arr.sort();
+	cout<<"Displaying array in sorted"<<endl;
+	arr.display();
+
+	arr.reverse();
+	cout<<"Displaying array in reverse"<<endl;
+	arr.display();
+
+	
+}
